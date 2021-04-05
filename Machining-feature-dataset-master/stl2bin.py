9e274f3eaa0a6e59df00dfcf07037f2ad820bc79
cotterp <philip.d.cotter@gmail.com>
2021-04-05 10:34:35 -0400

    binbox and viewvox commands

--- Machining-feature-dataset-master/stl2bin.py
+++ Machining-feature-dataset-master/stl2bin.py
@@ -1,1 +1,7 @@
 # placeholder script for converting stl files into bin files for input into new_input_processor.py
+
+# command to run binvox and convert STL to binvox (voxelizing the model)
+# ./binvox 0_1.STL -d 64 -e
+
+# command to view a binvox file and confirm the voxelization makes sense
+# ./viewvox 0_1.binvox
